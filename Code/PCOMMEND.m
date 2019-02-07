69c3e107a0f7db38215f61d930ce04dac2e8b2cf
Yutai <yutai.20m5@gmail.com>
2019-02-07 16:24:51 -0500

    temporarily modified to use same set of saved random data. hoping to debug python

--- Code/PCOMMEND.m
+++ Code/PCOMMEND.m
@@ -44,7 +44,8 @@ function [E,P,U]=PCOMMEND(X,parameters)
 %%
 
 % Initialize parameters
-[E,U] = PCOMMEND_Initialize(X,parameters.C,parameters.M);
+% [E,U] = PCOMMEND_Initialize(X,parameters.C,parameters.M);
+load deterministic_data_for_sim_data_python_debug.mat X E U
 [N,D] = size(X); 
 Cond_old = inf;
 
